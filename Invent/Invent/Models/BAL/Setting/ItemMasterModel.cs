374ef6b2ed46e3c4f6e4a434553a78b2fa52c341
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2018-11-26 18:45:30 +0530

    Changes

--- Invent/Invent/Models/BAL/Setting/ItemMasterModel.cs
+++ Invent/Invent/Models/BAL/Setting/ItemMasterModel.cs
@@ -16,6 +16,7 @@ namespace Invent.Models.BAL.Setting
         public void SaveItemsMaster(string userId, string jsonData, string flag)
         {
             ResponseEntity error = ResponseEntity.GetInstance();
+            DataSet ds = new DataSet();
             SqlParameter[] sqlParameter = new SqlParameter[5];
             sqlParameter[0] = new SqlParameter("@USER_ID", userId);
             sqlParameter[1] = new SqlParameter("@JSON_DATA", jsonData);
@@ -26,7 +27,7 @@ namespace Invent.Models.BAL.Setting
             sqlParameter[4] = new SqlParameter("@ERROR_MSG", SqlDbType.NVarChar);
             sqlParameter[4].Direction = ParameterDirection.Output;
             sqlParameter[4].Size = 100;
-            SqlHelper.ExecuteNonQuery(sqlconn, CommandType.StoredProcedure, "SP_MANAGE_ITEM_MASTER", sqlParameter);
+            ds=SqlHelper.ExecuteDataset(sqlconn, CommandType.StoredProcedure, "SP_MANAGE_ITEM_MASTER", sqlParameter);
             error.ERROR_FLAG = sqlParameter[3].Value.ToString();
             error.ERROR_MSG = sqlParameter[4].Value.ToString();
         }
